ce4b648df231aabdcca09194bb6c1b81e4374517
ElenaBiagi <elena.biagi24@gmail.com>
2025-03-10 15:23:13 +0100

    Add test

--- stats/tests_m.sh
+++ stats/tests_m.sh
@@ -29,24 +29,27 @@ do
     do
         echo "t=$t"
 
-        /usr/bin/time -l /Users/biagiele/try/Pseudoalignment-Finimizers/benchmark search-fmin -i /Users/biagiele/try/Pseudoalignment-Finimizers/results/Salmonella_concat_31 -q /Users/biagiele/try/queries/m_queries_Salmonella_100_200_${i}_${m}.fa -t $t -o /Users/biagiele/try/Pseudoalignment-Finimizers/stats/m_out_fi_100_200_${i}_$t_$m 1> /Users/biagiele/try/Pseudoalignment-Finimizers/results/search_Salmonella-unitigs-k31.stdout 2> /Users/biagiele/try/Pseudoalignment-Finimizers/results/search_Salmonella-unitigs-k31.stderr
+        /usr/bin/time -l /Users/biagiele/try/Pseudoalignment-Finimizers/benchmark search-fmin -i /Users/biagiele/try/Pseudoalignment-Finimizers/results/Salmonella_concat_31 -q /Users/biagiele/try/queries/m_queries_Salmonella_100_200_${i}_${m}.fa -t $t -o /Users/biagiele/try/Pseudoalignment-Finimizers/stats/m_out_fi_100_200_${i}_$t_$m.txt 1> /Users/biagiele/try/Pseudoalignment-Finimizers/results/search_Salmonella-unitigs-k31.stdout 2> /Users/biagiele/try/Pseudoalignment-Finimizers/results/search_Salmonella-unitigs-k31.stderr
     done
+
+    echo "3. Local alignment with BLAST"
+    blastn -db /Users/biagiele/try/Salmonella/db/Salmonella_db -query /Users/biagiele/try/queries/m_queries_Salmonella_100_200_1_0.01.fa -out /Users/biagiele/try/blast/m_out_b_100_200_${i}_$m.txt
     #rm /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_fi_100_200_${i}
     # Run Themisto
     #bash /Users/biagiele/try/themisto/run_themisto.sh
-    echo "3. Pseudoalignment with Themisto"
+    #echo "3. Pseudoalignment with Themisto"
     #/Users/biagiele/try/themisto/build/bin/themisto pseudoalign --query-file /Users/biagiele/try/Pseudoalignment-Finimizers/Salmonella/queries/queries_Salmonella_100_200_${i}.fa --index-prefix /Users/biagiele/try/themisto/my_index --temp-dir /Users/biagiele/try/themisto/temp --out-file /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_t_100_200_${i}.txt --n-threads 1 --threshold 0.7
 
 
     # Extract statistics
     echo "4. Extract statistics"
     # t = 0.8
-    python3 /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats.py /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_t_100_200_${i}.txt /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_f_100_200_${i}.stats 43 >> /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats_100_200.txt
-    python3 /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats2.py /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_t_100_200_${i}.txt /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_f_100_200_${i}.stats 43 >> /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats_100_200.txt
+    #python3 /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats.py /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_t_100_200_${i}.txt /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_f_100_200_${i}.stats 43 >> /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats_100_200.txt
+    #python3 /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats2.py /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_t_100_200_${i}.txt /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_f_100_200_${i}.stats 43 >> /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats_100_200.txt
     
     # intersection
-    python3 /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats.py /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_t_100_200_${i}.txt /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_fi_100_200_${i}.stats 43 >> /Users/biagiele/try/Pseudoalignment-Finimizers/stats/int_stats_100_200.txt
-    python3 /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats2.py /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_t_100_200_${i}.txt /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_fi_100_200_${i}.stats 43 >> /Users/biagiele/try/Pseudoalignment-Finimizers/stats/int_stats_100_200.txt
+    #python3 /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats.py /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_t_100_200_${i}.txt /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_fi_100_200_${i}.stats 43 >> /Users/biagiele/try/Pseudoalignment-Finimizers/stats/int_stats_100_200.txt
+    #python3 /Users/biagiele/try/Pseudoalignment-Finimizers/stats/stats2.py /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_t_100_200_${i}.txt /Users/biagiele/try/Pseudoalignment-Finimizers/stats/out_fi_100_200_${i}.stats 43 >> /Users/biagiele/try/Pseudoalignment-Finimizers/stats/int_stats_100_200.txt
 
     echo "DONE $i"
 done
