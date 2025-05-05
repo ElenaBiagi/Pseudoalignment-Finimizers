ea0d7436678b01b6335d16ec6277e20b8e2f41ef
ElenaBiagi <elena.biagi24@gmail.com>
2025-05-05 14:25:54 +0300

    Fix i and m

--- extract_queries.sh
+++ extract_queries.sh
@@ -1,3 +1,6 @@
+folder=$1
+m=0.01
+
 if [ ! -d "$folder/queries" ]; then
     mkdir ${folder}/queries
 fi  
@@ -5,8 +8,8 @@ fi
 for i in $(seq 1 10);
 do
     echo "1. Sample positive queries ($i)"
-    $HOME/Pseudoalignment-Finimizers/sample_random $HOME/${folder}/${folder}_concat.fa 1000 200 i > $HOME/${folder}/queries/queries_${folder}_1000_200_${i}.fa 
+    $HOME/Pseudoalignment-Finimizers/sample_random $HOME/${folder}/${folder}_concat.fa 1000 200 $i > $HOME/${folder}/queries/queries_${folder}_1000_200_${i}.fa 
 
     echo "2. Mutate queries"
-    $HOME/Pseudoalignment-Finimizers/mutate_seq $HOME/queries/queries_${folder}_1000_200_${i}.fa $HOME/${folder}/queries/m_queries_${folder}_1000_200_${i}_ $m i
+    $HOME/Pseudoalignment-Finimizers/mutate_seq $HOME/queries/queries_${folder}_1000_200_${i}.fa $HOME/${folder}/queries/m_queries_${folder}_1000_200_${i}_ $m $i
 done
