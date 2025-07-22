81179cf6ca0170f41de0db1c0b0d8d6cdb9d1f20
ElenaBiagi <elena.biagi24@gmail.com>
2025-07-22 12:08:31 +0300

    Add extract_queries.sh (to be deleted)

--- extract_queries.sh
+++ extract_queries.sh
@@ -1,14 +1,30 @@
-folder=$1
+#!/bin/bash
+#SBATCH --job-name=queries_Human.job
+#SBATCH -p short
+#SBATCH -M kale
+#SBATCH --chdir=/home/biagiele/Pseudoalignment-Finimizers/
+#SBATCH --mem=370G
+#SBATCH -t03:00:00
+#SBATCH -n1
+#SBATCH -c1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=elena.biagi@helsinki.fi
+
+
+name=$1
+folder=$2
 m=0.01
 
-if [ ! -d "$folder/queries" ]; then
-    mkdir ${folder}/queries
+if [ ! -d "$HOME/$folder/queries" ]; then
+    mkdir $HOME/${folder}/queries
 fi  
 
-for i in $(seq 1 10);
+for i in $(seq 6 10);
 do
     echo "1. Sample positive queries ($i)"
-    $HOME/Pseudoalignment-Finimizers/sample_random $HOME/${folder}/${folder}_concat.fa 10000 1000 $i > $HOME/${folder}/queries/queries_${folder}_10000_1000_${i}.fa 
+    $HOME/Pseudoalignment-Finimizers/sample_random $HOME/${folder}/${name}_concat.fa 10000 1000 $i > $HOME/${folder}/queries/queries_${name}_10000_1000_${i}.fa 
+
+    #$HOME/Pseudoalignment-Finimizers/sample_random $HOME/${folder}/seq/${name}_num_concat.fna 10000 1000 $i > $HOME/${folder}/queries/new_queries_${name}_10000_1000_${i}.fa 
     #$HOME/Pseudoalignment-Finimizers/sample_random $HOME/norm-hybrid-files/assemblies/hybrid/seq/TE_num_concat_blast.fna 10000 1000 $i > $HOME/${folder}/queries/queries_TE_10000_1000_${i}.fa 
 
     #echo "2. Mutate queries"
