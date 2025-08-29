0d12d47c702844dd2e2bcd8f8aa584ccee5f2335
ElenaBiagi <elena.biagi24@gmail.com>
2025-08-29 14:10:34 +0300

    Modified output buffer

--- include/search_fmin.hh
+++ include/search_fmin.hh
@@ -24,98 +24,125 @@ int64_t run_fmin_queries_streaming(reader_t& reader, out_stream_t& out, const Co
     int i=0;
     const size_t flush_t = 8 * 1024 * 1024; // 8 MB //1 << 20; // 1MB
 
+    //string buffer;
+    //buffer.reserve(flush_t);
+
+    size_t buffer_size = 0;
+    std::ostringstream buffer;
+
     if (t > 0){
-        std::ostringstream buffer;
-        size_t buffer_size = 0;
         
         while(true){
             int64_t len = reader.get_next_read_to_buffer();
-
             if(len == 0) break;
-            //out << i << " ";
+            
             string i_str = to_string(i);
+            buffer << i_str << " ";
+            buffer_size += i_str.size() + 1;
 
-            buffer << i_str << " " ;
-            buffer_size += i_str.size()+1;
+            //buffer.append(to_string(i)).push_back(':');
+            //buffer_size += to_string(i).size()+1; 
 
             //int64_t t0 = cur_time_micros();
             //string seq = remove_N_from_string(reader.read_buf);
-            string seq = reader.read_buf;
+            const string& seq = reader.read_buf;
 
             vector<pair<uint16_t, uint16_t>> ans;
+            ans.reserve(seq.size());
+
             const uint16_t min_value = index.search(seq, ans, t );
-            
+
             auto start = std::chrono::high_resolution_clock::now();
             for (int a = static_cast<int>(ans.size()) - 1; a >= 0; a--) {
 
                 const auto& [idx, count] = ans[a];
                 if (count < min_value){break;}
-                //out << idx << ":" << count << " ";
-                string pair_str = std::to_string(idx) + ":" + std::to_string(count) + " ";
-                buffer << pair_str;
-                buffer_size += pair_str.size();
+
+                // buffer.append(to_string(idx)).push_back(':');
+                // buffer.append(to_string(count)).push_back(' ');
+                buffer << idx << ":" << count << " ";
+                buffer_size += to_string(idx).size() + to_string(count).size() + 2; // 2 for ':', ' '
 
             }
-            //out << '\n';
-            string s_end = "\n";
-            buffer << s_end;
-            buffer_size += s_end.size();
+            //buffer.push_back('\n');
+            buffer << '\n';
+            buffer_size += 1;
 
             if (buffer_size >= flush_t) {
-                out << buffer.str();
-                buffer.str("");
+                string tmp = move(buffer).str();   
+                out.write(tmp.data(), tmp.size());
+                //out.write(buffer.data(), buffer.size());
                 buffer.clear();
                 buffer_size = 0;
             }
+
             i++;
             auto end = std::chrono::high_resolution_clock::now();
             time_output += (end - start);
         }
-
-        auto start = std::chrono::high_resolution_clock::now();
-        if (buffer_size > 0) {
-            out << buffer.str();
-        }
-        auto end = std::chrono::high_resolution_clock::now();
-        time_output+= (end - start);
-
-    } else {
+    } else { // Print everything 
         while(true){
-            // TODO PRINT OUTPUT ONLY AT THE END?
-
             int64_t len = reader.get_next_read_to_buffer();
-
             if(len == 0) break;
-            out << i << " ";
+
+            //buffer.append(to_string(i)).push_back(' ');
+            string i_str = to_string(i);
+            buffer << i_str << " ";
+            buffer_size += to_string(i).size() + 1;
 
             //int64_t t0 = cur_time_micros();
             //string seq = remove_N_from_string(reader.read_buf);
-            string seq = reader.read_buf;
+            const string& seq = reader.read_buf;
 
             vector<pair<uint16_t, uint16_t>> ans;
+            ans.reserve(seq.size());
             index.search(seq, ans);
-
+            
             auto start = std::chrono::high_resolution_clock::now();
-
             for (int a = static_cast<int>(ans.size()) - 1; a >= 0; a--) {
 
                 const auto& [idx, count] = ans[a];
                 if (count == 0){break;}
-                out << idx << ":" << count << " ";
+                //buffer.append(to_string(idx)).push_back(':');
+                //buffer.append(to_string(count)).push_back(' ');
+                buffer << idx << ":" << count << " ";
+                buffer_size += to_string(idx).size() + to_string(count).size() + 2;
+
             }
-            out << '\n';
+            //buffer.push_back('\n');
+            buffer << '\n';
+            buffer_size += 1;
+
+            if (buffer_size >= flush_t) {
+                string tmp = move(buffer).str();   
+                out.write(tmp.data(), tmp.size());
+                //out.write(buffer.data(), buffer.size());
+                buffer.clear();
+                buffer_size = 0;
+            }
+
             i++;
             auto end = std::chrono::high_resolution_clock::now();
-            time_output+= (end - start);
+            time_output += (end - start);
+
         }
 
     }
+    auto start = std::chrono::high_resolution_clock::now();
+
+    if (buffer_size > 0) {
+            //out.write(buffer.data(), buffer.size());
+            string tmp = move(buffer).str();   
+            out.write(tmp.data(), tmp.size());
+        }
+    
+    auto end = std::chrono::high_resolution_clock::now();
+    time_output += (end - start);
 
     //total_micros += cur_time_micros() - t0;
     //write_log("k " + to_string(k), LogLevel::MAJOR);
     //write_log("us/query: " + to_string((double)total_micros / number_of_queries) + " (excluding I/O etc)", LogLevel::MAJOR);
     print_search_timing_stats();
-
     return 1;
 }
 
@@ -222,12 +249,14 @@ int search_fmin(int argc, char** argv){
     /* int64_t total_micros = 0; 
     int64_t t0 = cur_time_micros();
     */
-
-    auto start = std::chrono::high_resolution_clock::now();
-    CompressedColoredFinimizers index;
-    index.load(index_prefix);
-    auto end = std::chrono::high_resolution_clock::now();
-    time_index_loading += (end - start);
+   CompressedColoredFinimizers index;
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+    index.load(index_prefix);            
+        auto end = std::chrono::high_resolution_clock::now();
+        time_index_loading += (end - start);
+    }
+    
     /* total_micros += cur_time_micros() - t0;
     cerr << total_micros << endl; */
     cerr << "Index loaded" << endl;
