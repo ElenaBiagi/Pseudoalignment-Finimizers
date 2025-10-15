0f38550a37fbb08f31de04ce8a932fdb8555b910
Jarno N. Alanko <alanko.jarno@gmail.com>
2025-10-15 16:31:10 +0300

    WIP

--- include/Color_Set.hh
+++ include/Color_Set.hh
@@ -295,14 +295,14 @@ class SDSL_Variant_Color_Set{
     vector<int64_t> get_colors_as_vector() const {return colorset_get_colors_as_vector(*this);}
     void push_colors_to_vector(vector<int64_t>& vec) const {return colorset_push_colors_to_vector(*this, vec);}
 
-    void increment_color_counters(vector<int64_t>& counts) const {
+    void increment_color_counters(vector<int64_t>& counts, vector<int64_t>& non_zero_count_indices) const {
 
         auto visitor = [](auto&& arg) {
             using T = std::decay_t<decltype(arg)>;
 
             if constexpr (std::is_same_v<T, bit_vector*>) {
                 // Increment counters 
-                //std::cout << "It's an int: " << arg << '\n';
+                // If count grows from 0 to 1, push the index of the color to non_zero_count_indices
             }
             else if constexpr (std::is_same_v<T, int_vector*>) {
                 // Increment counters 
