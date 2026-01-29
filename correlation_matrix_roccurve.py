7b15bfc02e0eef4da4c7a0bce005d9d42f490373
ElenaBiagi <elena.biagi24@gmail.com>
2026-01-29 14:37:16 +0200

    add plots

--- correlation_matrix_roccurve.py
+++ correlation_matrix_roccurve.py
@@ -6,34 +6,13 @@ import argparse
 import sys
 from collections import defaultdict
 import numpy as np
-from sklearn.metrics import matthews_corrcoef
+import gzip
+from sklearn.metrics import matthews_corrcoef, roc_curve, auc, precision_recall_curve, average_precision_score
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument("query_n", help="Enter the query name", type=str)
-
-def save_scatter_pdf(
-    x, y, filename,
-    xlabel="X",
-    ylabel="Y",
-    point_size=8,
-    point_color="steelblue"
-):
-    assert len(x) == len(y)
-
-    plt.figure(figsize=(6, 6))
-
-    plt.scatter(x, y, s=point_size, color=point_color)
-
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-
-    plt.grid(True, linestyle="--", linewidth=0.5, alpha=0.7)
-
-    plt.tight_layout()
-    plt.savefig(filename, format="pdf")
-    plt.close()
-               
+             
 def fill_minimap_hm(query_name, max_matches):
     """
     For each genome, read its PAF file for the query_name.
@@ -109,11 +88,8 @@ def fill_minimap_hm(query_name, max_matches):
                 #     cerr << f"Warning: total matches {total_matches} != value {value} in {paf} for read {query_idx}\n"
                 minimap_res[(query_idx, genome-1)] = max(minimap_res[(query_idx, genome-1)], value)
                 # minimap_res[(query_idx, genome-1)] = minimap_res[(query_idx, genome-1)] + value
-
-                
+             
     return minimap_res
-
-import gzip
 
 def fastq_gz_lengths(path):
     lengths = []
@@ -131,8 +107,8 @@ def fastq_gz_lengths(path):
 def main():
     n_cols = 3682       # genomes
     n_tools = 4         # themisto, finimap, kaminari, minimap
-    t = 0.8             # threshold
-    
+    tt = 0.8            # threshold
+    print("Themisto Threshold:", tt)
     args = parser.parse_args()
     query_n = args.query_n
     print("query_name =", query_n, flush=True)
@@ -141,28 +117,29 @@ def main():
         """ERR10496621, "ERR10496620", "ERR10496619", "ERR10496618",
             "ERR10496617", "ERR10496616", "ERR10496615", "ERR10496755",
             "ERR10496623", "ERR10496622"
-        ]:
         """
         # ------------------------------------------
         # READ ALL LINES OF ALL 4 FILES FIRST
         # ------------------------------------------
 
         # THEMISTO --------------------------------
-        print("read themisto", flush=True)
-        themisto_file = f"/home/biagiele/Ecoli/reads/results/{query_name}_themisto2_res_t{t}.txt"
+        # print("read themisto", flush=True)
+        themisto_file = f"/home/biagiele/Ecoli/reads/results/{query_name}_themisto2_res_t{tt}.txt"
         with open(themisto_file) as f:
             themisto_lines = [line.strip() for line in f]
 
+        tf = 0.7 
+        print("F & K Threshold:", tf)
         # FINIMAP ---------------------------------
-        print("read finimap", flush=True)
-        finimap_file = f"/home/biagiele/Ecoli/reads/results/bases_{query_name}_10_fmin_Ecoli_{t}.txt"
+        # print("read finimap", flush=True)
+        finimap_file = f"/home/biagiele/Ecoli/reads/results/bases_{query_name}_10_fmin_Ecoli_{tf}.txt"
         with open(finimap_file, "r") as f:
             finimap_lines = [line.strip() for line in f]
 
         # KAMINARI ---------------------------------        
-        print("read kaminari", flush=True)
-        # kaminari_file = f"/home/biagiele/Ecoli/reads/results/{query_name}_kaminari_res_t0.00000001_19.txt"
-        kaminari_file = f"/home/biagiele/Ecoli/reads/results/{query_name}_kaminari_res_t{t}_19.txt"
+        # print("read kaminari", flush=True)
+        #kaminari_file = f"/home/biagiele/Ecoli/reads/results/{query_name}_kaminari_res_t0.00000001_19.txt"
+        kaminari_file = f"/home/biagiele/Ecoli/reads/results/{query_name}_kaminari_res_t{tf}_19.txt"
         with open(kaminari_file, "r") as f:
             kaminari_lines = [line.strip() for line in f]
 
@@ -174,7 +151,7 @@ def main():
         max_matches = []
         for r in range(len(query_lens)):
             max_matches.append([0]*query_lens[r])
-        print("read minimap", flush=True)
+        # print("read minimap", flush=True)
         minimap_hm = fill_minimap_hm(query_name, max_matches)
         
         
@@ -183,43 +160,13 @@ def main():
         # min(len(themisto_lines),
         #         len(finimap_lines),
         #         len(kaminari_lines))
-        print(R)
-                
-
+        print(R*n_cols)
         
         # ------------------------------------------
         # BUILD M FOR EACH ROW r
         # ------------------------------------------
     
         # Collect values
-        
-        scatter_x = []
-        scatter_fx = []
-        scatter_kx = []
-        scatter_fy = []
-        scatter_ky = []
-        scatter_y = []
-        
-        FP_t = 0
-        FP_f = 0
-        FP_k = 0
-        
-        FN_t = 0
-        FN_f = 0
-        FN_k = 0
-        
-        TN_t = 0
-        TN_f = 0
-        TN_k = 0
-        
-        TP_t = 0
-        TP_f = 0
-        TP_k = 0
-        
-        true_m = [0] * (R * n_cols) 
-        pred_t = [0] * (R * n_cols)
-        pred_f = [0] * (R * n_cols)
-        pred_k = [0] * (R * n_cols)
                 
         M = np.zeros((n_tools, n_cols*R), dtype=float)
 
@@ -235,12 +182,6 @@ def main():
             for genome_num, value in zip(genomes, bases):
                 #if genome_num < 800:
                 M[0, (r*n_cols)+genome_num] = value/query_lens[r] # divide by the read length
-                
-            # pairs = re.findall(r'(\d+):(\d+)', themisto_lines[r])
-            #for genome_num, value in pairs:
-            #    genome_num, value = int(genome_num), int(value)
-            #    if genome_num < n_cols:
-            #        M[0, genome_num] = value """
 
             # FINIMAP -------------------------------
             # print("Insert finimap values", flush=True)
@@ -261,204 +202,98 @@ def main():
         
             # MINIMAP -------------------------------
             # print("Insert minimap values", flush=True)
+            tm = 0
+            if r == 0: 
+                print("Minimap threshold:", tm)
             for genome_num in range(n_cols):
                 key = (f"{query_name}.{r+1}", genome_num)
                 if key in minimap_hm:
                     value = minimap_hm[key]
-                    M[3,(r*n_cols) + genome_num] = min(value/query_lens[r], 1) # divide by the read length
+                    M[3,(r*n_cols) + genome_num] = min(value/query_lens[r],1) # divide by the read length
 
-            # maximum match = denominator 
-            # max_matches.append(max(M[3,(r*n_cols): (r*n_cols) + 3681]))
-            # TODO: Calculate REAL max matches for every read
-            # max_matches=max(M[3,(r*n_cols): (r*n_cols) + n_cols])
             minimap_threshold = sum(max_matches[r])/query_lens[r]
 
             # Remove values below the threshold
-            M[3, (r * n_cols): (r * n_cols) + n_cols] [M[3, (r * n_cols): (r * n_cols) + n_cols] < (minimap_threshold * t)] = 0
+            # M[3, (r * n_cols): (r * n_cols) + n_cols] [M[3, (r * n_cols): (r * n_cols) + n_cols] < (minimap_threshold * t)] = 0
+            # # Set everything else to 1
+            # M[3, (r * n_cols): (r * n_cols) + n_cols] [M[3, (r * n_cols): (r * n_cols) + n_cols] != 0] = 1
+            start = r * n_cols
+            end = start + n_cols
 
-            for genome in range(n_cols):
-                tx = M[0, (r*n_cols) + genome]   # Themisto
-                fx = M[1, (r*n_cols) + genome]   # Finimap
-                kx = M[2, (r*n_cols) + genome]   # Kaminari
-                my = M[3, (r*n_cols) + genome]   # Minimap2
-            
-                if my > 0:
-                    true_m[(r * n_cols) + genome] = 1
-                
-                if tx > 0:
-                    pred_t[(r * n_cols) + genome] = 1
-                
-                if fx > 0:
-                    pred_f[(r * n_cols) + genome] = 1
-                
-                if kx > 0:
-                    pred_k[(r * n_cols) + genome] = 1
-                
-                # FALSE POSITIVES
-                if my > 0:
-                    # False negatives
-                    if tx == 0:
-                        FN_t+=1
-                    if fx == 0:
-                        FN_f+=1
-                    if kx == 0:
-                        FN_k+=1
-                    # True positives
-                    if tx > 0:
-                        TP_t+=1
-                    if fx > 0:
-                        TP_f+=1
-                    if kx > 0:
-                        TP_k+=1
-                        
-                elif my == 0:
-                    # true negatives
-                    if tx == 0:
-                        TN_t+=1
-                    if fx == 0:
-                        TN_f+=1
-                    if kx == 0:
-                        TN_k+=1
-                    # False positives
-                    if tx > 0:
-                        FP_t+=1
-                    if fx > 0:
-                        FP_f+=1
-                    if kx > 0:
-                        FP_k+=1
-                
-                
-                # scatter plot values
-                if tx > 0 or my > 0:   # avoid empty points
-                    scatter_x.append(tx)
-                    scatter_y.append(my)
-                if fx > 0 or my > 0:   # avoid empty points
-                    scatter_fx.append(fx)
-                    scatter_fy.append(my)
-                if kx > 0 or my > 0:   # avoid empty points
-                    scatter_kx.append(kx)
-                    scatter_ky.append(my)
-                                
+            M[3, start:end] = (M[3, start:end] >= (minimap_threshold * tm)).astype(int)
     # --------------------------------------
     # CORRELATION MATRIX
     # --------------------------------------
-    # print(true_m)
-    # print(pred_t)
-    # print(pred_f)
-    print(matthews_corrcoef(true_m, pred_t))
-    print(matthews_corrcoef(true_m, pred_f))
-    print(matthews_corrcoef(true_m, pred_k))
+    # print(matthews_corrcoef(true_m, pred_t))
+    # print(matthews_corrcoef(true_m, pred_f))
+    # print(matthews_corrcoef(true_m, pred_k))
 
-    # Save M[0,:] Themisto
-    with open(f"/home/biagiele/Ecoli/reads/results/{query_name}_themisto2_list_t{t}.txt", "w") as f:
-        for value in M[0, :]:
-            f.write(f"{value}\n")
-    # Save M[1,:] Finimap
-    with open(f"/home/biagiele/Ecoli/reads/results/{query_name}_finimap_list_t{t}.txt", "w") as f:
-        for value in M[1, :]:
-            f.write(f"{value}\n")
-    # Save M[2,:] kaminari
-    with open(f"/home/biagiele/Ecoli/reads/results/{query_name}_kaminari_list_t{t}.txt", "w") as f:
-        for value in M[2, :]:
-            f.write(f"{value}\n")
-    # Save M[3,:] Minimap
-    with open(f"/home/biagiele/Ecoli/reads/results/{query_name}_minimap_list_t{t}.txt", "w") as f:
-        for value in M[3, :]:
-            f.write(f"{value}\n")     
+    # corr = np.corrcoef(M)
+    
+    # print(f"Correlation matrix for {query_name}")
+    # print(corr)
+    # print()
+    labels = M[3, :] # Minimap2 
+    
+    tools = {
+        "Themisto": M[0, :],
+        "Finimap ": M[1, :],
+        "Kaminari": M[2, :],
+    }
 
+    plt.figure(figsize=(6, 6))
+    
+    print(f"#zero:{(labels==0).sum()}, #ones: {(labels==1).sum()}")
+
+    for name, scores in tools.items():
+        fpr, tpr, _ = roc_curve(labels, scores)
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, label=f"{name} (AUC={roc_auc:.3f})")
+
+    plt.plot([0, 1], [0, 1], "k--", alpha=0.6)
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC Curves")
+    plt.legend(loc="lower right")
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.savefig(f"roc_all_tools_{query_n}_{tt}-{tf}-{tm}.pdf")
+    plt.close()
+    
+    
+    plt.figure(figsize=(6, 6))
+
+    for name, scores in tools.items():
+        precision, recall, _ = precision_recall_curve(labels, scores)
+        ap = average_precision_score(labels, scores)
+        plt.plot(recall, precision, label=f"{name} (AP={ap:.3f})")
+
+    # Baseline = positive prevalence
+    baseline = labels.mean()
+    plt.hlines(baseline, 0, 1, linestyles="dashed", colors="gray",
+            label=f"Baseline ({baseline:.3f})")
+
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.title("Precision–Recall Curves")
+    plt.legend(loc="upper right")
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.savefig(f"pr_all_tools_{query_n}_{tt}-{tf}-{tm}.pdf")
+    plt.close()
+    
+    print("Positive rate:", labels.mean())
+    print("Minimap:", labels.mean() * R * n_cols)
+    for name, scores in tools.items():
+        print(name, np.count_nonzero(scores))
+    
     corr = np.corrcoef(M)
     
     print(f"Correlation matrix for {query_name}")
     print(corr)
     print()
-            
-    if scatter_x:
-        save_scatter_pdf(
-            scatter_x,
-            scatter_y,
-            "norm_{query_name}_themisto_vs_minimap2_0.8.pdf",
-            xlabel="Themisto",
-            ylabel="Minimap2"
-        )
-        print(max(scatter_x))
-        print(max(scatter_y))
-    if scatter_fx:
-        save_scatter_pdf(
-            scatter_fx,
-            scatter_fy,
-            f"norm_{query_name}_finimap_vs_minimap2_0.8.pdf",
-            xlabel="Finimap",
-            ylabel="Minimap2",
-            point_color="chocolate",#(210, 105, 30), #
-        )
-        print(max(scatter_fx))
-        print(max(scatter_fy))
-    if scatter_kx:
-        save_scatter_pdf(
-            scatter_kx,
-            scatter_ky,
-            f"norm_{query_name}_kaminari_vs_minimap2_0.8.pdf",
-            xlabel="Kaminari",
-            ylabel="Minimap2",
-            point_color="red",
-        )
-        print(max(scatter_kx))
-        print(max(scatter_ky))
-    
-    
-    print()
-    print(TP_t)
-    print(TP_f)
-    print(TP_k)
-    print()
-    print(FP_t)
-    print(FP_f)
-    print(FP_k)
-    print()
-    print(FN_t)
-    print(FN_f)
-    print(FN_k)
-    print()
-    print(TN_t)
-    print(TN_f)
-    print(TN_k)
-    
-    print()
-    print("False positive rate = FP/(FP+TN)")
-    print(FP_t/(FP_t+TN_t))
-    print(FP_f/(FP_f+TN_f))
-    print(FP_k/(FP_t+TN_t))
-    
-    print("'%' false positives")
-    print(FP_t/(R*n_cols))
-    print(FP_f/(R*n_cols))
-    print(FP_k/(R*n_cols))
-    
-    print("False negative rate = FN/(TP+FN)")
-    print(FN_t/(FN_t+TP_t))
-    print(FN_f/(FN_f+TP_f))
-    print(FN_k/(FN_t+TP_t))
-    
-    print("'%' false negatives")
-    print(FN_t/(R*n_cols))
-    print(FN_f/(R*n_cols))
-    print(FN_k/(R*n_cols))
-    
-    print("Accuracy")
-    print((TP_t+TN_t)/(R*n_cols))
-    print((TP_f+TN_f)/(R*n_cols))
-    print((TP_k+TN_k)/(R*n_cols))
-    
-    print("Precision")
-    print(TP_t/(TP_t +FN_t))
-    print(TP_f/(TP_f +FN_f))
-    print(TP_k/(TP_k +FN_k))
-    
-    print("Recall")
-    print(TP_t/(FN_t+TP_t))
-    print(TP_f/(FN_f+TP_f))
-    print(TP_k/(FN_t+TP_t))
-    
 
 if __name__ == "__main__":
     main()
